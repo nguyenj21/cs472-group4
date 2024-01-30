ca9ab58aa8efdd84ad13007c40fbbf5f6b3ab2e3
39otsu <mccodi11@gmail.com>
2024-01-29 23:58:21 -0800

    removed thing

--- repo_mining/codilla_CollectFiles.py
+++ repo_mining/codilla_CollectFiles.py
@@ -64,7 +64,7 @@ repo = 'scottyab/rootbeer'
 # Remember to empty the list when going to commit to GitHub.
 # Otherwise they will all be reverted and you will have to re-create them
 # I would advise to create more than one token for repos with heavy commits
-lstTokens = ["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"]
+lstTokens = []
 
 dictfiles = dict()
 countfiles(dictfiles, lstTokens, repo)
