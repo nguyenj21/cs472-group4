2cca83eaab9c46dbccd9d6d285f018a0f2c0ab41
nguyenj21 <124472966+nguyenj21@users.noreply.github.com>
2024-01-29 22:38:16 -0800

    Update Johnson_scatterplot.py

--- repo_mining/Johnson_scatterplot.py
+++ repo_mining/Johnson_scatterplot.py
@@ -18,9 +18,9 @@ repo = 'scottyab/rootbeer'
 # Remember to empty the list when going to commit to GitHub.
 # Otherwise, they will all be reverted and you will have to re-create them
 # I would advise creating more than one token for repos with heavy commits
-lstTokens = ["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",
-              "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",
-              "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"]
+lstTokens = ["faketoken1",
+              "faketoken2",
+              "faketoken3"]
 
 # initializes dictonary for the files
 dictfiles = dict()
