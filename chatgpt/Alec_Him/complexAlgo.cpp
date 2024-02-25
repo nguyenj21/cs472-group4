437bfb4ed5d5d6ad57b2d7fe7813f5d433621c49
Redblaze <alecvhim@gmail.com>
2024-02-25 14:18:24 -0800

    Implemented ChatGPT suggestions and insight to optimize matrix multiplication program

--- chatgpt/Alec_Him/complexAlgo.cpp
+++ chatgpt/Alec_Him/complexAlgo.cpp
@@ -4,15 +4,14 @@
 */
 
 #include <iostream>
-
-using namespace std;
+#include <array>
 
 template <size_t rows, size_t cols>
-void matrixMult(int (&mat1)[rows][cols], int (&mat2)[rows][cols], int (&result)[rows][cols]){
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
+void matrixMult(const std::array<std::array<int, cols>, rows>& mat1, const std::array<std::array<int, cols>, rows>& mat2, std::array<std::array<int, cols>, rows>& result){
+    for(size_t i = 0; i < rows; i++){
+        for(size_t j = 0; j < cols; j++){
             result[i][j] = 0;
-            for(int k = 0; k < cols; k++){
+            for(size_t k = 0; k < cols; k++){
                 result[i][j] += mat1[i][k] * mat2[k][j];
             }
         }
@@ -20,35 +19,33 @@ void matrixMult(int (&mat1)[rows][cols], int (&mat2)[rows][cols], int (&result)[
 }
 
 template <size_t rows, size_t cols>
-void display(int (&matrix)[rows][cols]){
-    cout << "---------" << endl;
+void display(const std::array<std::array<int, cols>, rows>& matrix){
+    std::cout << "---------" << std::endl;
     for(int i = 0; i < rows; i++){
-        cout << "|";
+        std::cout << "|";
         for(int j = 0; j < cols; j++){
-            cout << " " << matrix[i][j] << " |";
+            std::cout << " " << matrix[i][j] << " |";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
-    cout << "---------" << endl;
+    std::cout << "---------" << std::endl;
 }
 
 int main(){
-    int matrix1[2][2] = { {1, 0},
-                          {0, 1} };
-    int matrix2[2][2] = { {2, 0},
-                          {0, 2} };
+    std::array<std::array<int, 2>, 2> matrix1 = {{{2, 3}, {3, 5}}};
+    std::array<std::array<int, 2>, 2> matrix2 = {{{2, 3}, {3, 5}}};
 
-    const int col1 = sizeof(matrix1[0]) / sizeof(matrix1[0][0]);
-    const int row2 = sizeof(matrix2) / sizeof(matrix2[0]);
+    constexpr size_t col1 = matrix1.size();
+    constexpr size_t row2 = matrix2[0].size();
                            
     // Check if the number of columns in matrix1 is equal to the number of rows in matrix2
     if (col1 != row2) {
-        cout << "The # of columns in matrix 1 is not equal to the # of rows in matrix 2. Unable to perform multiplication" << endl;
+        std::cout << "The number of columns in matrix 1 is not equal to the number of rows in matrix 2. Unable to perform multiplication" << std::endl;
     } else {
-        int result[col1][row2];
-        cout << "Performing Calculation..." << endl;
+        std::array<std::array<int, col1>, row2> result;
+        std::cout << "Performing Calculation..." << std::endl;
         matrixMult(matrix1, matrix2, result);
-        cout << "The value of the multiplication is: " << endl;
+        std::cout << "The value of the multiplication is: " << std::endl;
         display(result);
     }
 
