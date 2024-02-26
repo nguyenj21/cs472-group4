794754b24786841e86cf62f72c88d32562bc24af
salazw1 <salazw1@unlv.nevada.edu>
2024-02-25 17:58:52 -0800

    Refactored code snippet with ChatGPT suggestions

--- chatgpt/Warren_Salazar/Warren_Salazar_task2_1.cpp
+++ chatgpt/Warren_Salazar/Warren_Salazar_task2_1.cpp
@@ -3,37 +3,29 @@ using namespace std;
 
 const int ARRAY_SIZE = 4;
 
-void arrangeArray(double[], double, double);
+void findMinMax(double[], double&, double&);
 
 int main()
 {
     double list[ARRAY_SIZE] = {3.5, 1.6, -4.9, 2.5};
-    double minimum = 0.0, maximum = 0.0;
-    arrangeArray(list, minimum, maximum);
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        cout << list[i];
-        cout << endl;
-    }
+    double minimum = list[0];
+    double maximum = list[0];
+    findMinMax(list, minimum, maximum);
+    cout << "Minimum: " << minimum << endl;
+    cout << "Maximum: " << maximum << endl;
+    return 0;
 }
 
-void arrangeArray(double list[], double minimum, double maximum)
+void findMinMax(double list[], double& minimum, double& maximum)
 {
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    for (int i = 1; i < ARRAY_SIZE; i++)
     {
         if (list[i] < minimum){
             minimum = list[i];
         }
-    }
-    cout << "new minimum: " << minimum;
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        if (list[i] > maximum)
+        else if (list[i] > maximum)
         {
             maximum = list[i];
         }
     }
-    cout << "\nnew maximum: " << maximum;
-    cout << "\n";
 }
-
