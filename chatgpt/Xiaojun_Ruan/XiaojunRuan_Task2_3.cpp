384b786237295663c67900eba9c19b75adb0a706
junxjr <ruanxj02@gmail.com>
2024-02-25 22:32:52 -0800

    Xiaojun Ruan Task 2.3 Code Complexity Fixed

--- chatgpt/Xiaojun_Ruan/XiaojunRuan_Task2_3.cpp
+++ chatgpt/Xiaojun_Ruan/XiaojunRuan_Task2_3.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
 
-// Function to calculate the area of a rectangle
-double calculateRectangleArea(double length, double width) {
-    return length * width;
-}
+void printRectangleInformation(double length, double width, double height) {
+    double area = length * width;
+    double perimeter = 2 * (length + width);
+    double volume = length * width * height;
 
-// Function to calculate the perimeter of a rectangle
-double calculateRectanglePerimeter(double length, double width) {
-    return 2 * (length + width);
-}
-
-// Function to calculate the volume of a rectangular prism
-double calculateRectangleVolume(double length, double width, double height) {
-    return length * width * height;
-}
-
-// Function to print information about a rectangle (area, perimeter, volume)
-void printRectangleInformation(double area, double perimeter, double volume) {
     std::cout << "Rectangle Area: " << area << std::endl;
     std::cout << "Rectangle Perimeter: " << perimeter << std::endl;
     std::cout << "Rectangle Volume: " << volume << std::endl;
@@ -28,26 +16,18 @@ int main() {
     double width1 = 4;
     double height1 = 3;
 
-    // Calculate area, perimeter, and volume of the first rectangle
-    double area1 = calculateRectangleArea(length1, width1);
-    double perimeter1 = calculateRectanglePerimeter(length1, width1);
-    double volume1 = calculateRectangleVolume(length1, width1, height1);
-
     // Print information about the first rectangle
-    printRectangleInformation(area1, perimeter1, volume1);
+    std::cout << "First Rectangle:" << std::endl;
+    printRectangleInformation(length1, width1, height1);
 
     // Define the dimensions of the second rectangle
     double length2 = 6;
     double width2 = 9;
     double height2 = 10;
 
-    // Calculate area, perimeter, and volume of the second rectangle
-    double area2 = calculateRectangleArea(length2, width2);
-    double perimeter2 = calculateRectanglePerimeter(length2, width2);
-    double volume2 = calculateRectangleVolume(length2, width2, height2);
-
     // Print information about the second rectangle
-    printRectangleInformation(area2, perimeter2, volume2);
+    std::cout << "Second Rectangle:" << std::endl;
+    printRectangleInformation(length2, width2, height2);
 
     return 0;
 }
